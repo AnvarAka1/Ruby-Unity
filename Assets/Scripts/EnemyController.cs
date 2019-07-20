1a075e295c3b87c03e2c5646efa4716a3d7fce71
Anvar <Anvar-abd_97@mail.ru>
2019-07-20 21:33:46 +0500

    final backup

---
+++ Assets/Scripts/EnemyController.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyController : MonoBehaviour
+{
+    public ParticleSystem smokeEffect;
+
+    public float speed = 3f;
+    public bool vertical;
+    public float changeTime = 3f;
+
+    Rigidbody2D rigidbody2d;
+    Animator animator;
+
+    float timer;
+    int direction = 1;
+    bool broken = true;
+    void Start()
+    {
+        rigidbody2d = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+        timer = changeTime;
+    }
+
+    void Update()
+    {
+        if (!broken)
+        {
+            return;
+        }
+        timer -= Time.deltaTime;
+        if (timer < 0)
+        {
+            direction = -direction;
+            timer = changeTime;
+
+        }
+        Vector2 position = rigidbody2d.position;
+        if (vertical)
+        {
+            animator.SetFloat("MoveX", 0);
+            animator.SetFloat("MoveY", direction);
+            position.y = position.y + speed * direction * Time.deltaTime;
+        }
+        else
+        {
+            animator.SetFloat("MoveX", direction);
+            animator.SetFloat("MoveY", 0);
+            position.x = position.x + speed * direction * Time.deltaTime;
+        }
+
+        rigidbody2d.MovePosition(position);
+    }
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        RubyController player = other.gameObject.GetComponent<RubyController>();
+        if (player != null)
+        {
+            player.ChangeHealth(-1);
+        }
+    }
+
+    public void Fix()
+    {
+        broken = false;
+        rigidbody2d.simulated = false;
+        animator.SetTrigger("Fixed");
+        smokeEffect.Stop();
+    }
+}
